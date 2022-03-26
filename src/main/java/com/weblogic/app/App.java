133cbce5b42f836ff8b5b48a7092daaebfb8d047
nagaraju531 <nraju531@gmail.com>
2022-03-26 17:11:13 +0530

    Update App.java

--- src/main/java/com/weblogic/app/App.java
+++ src/main/java/com/weblogic/app/App.java
@@ -7,8 +7,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        # Removing this comment line for testing
-        # Adding extra comment line
+        
         System.out.println("The Title is from raju");
     }
 }
