66f94630c0328f2f37e2dde274ba1098f995f0c8
nagaraju531 <nraju531@gmail.com>
2022-03-26 18:06:12 +0530

    Update App.java

--- src/main/java/com/weblogic/app/App.java
+++ src/main/java/com/weblogic/app/App.java
@@ -8,6 +8,6 @@ public class App
     public static void main( String[] args )
     {
         
-        System.out.println("The Title is from raju");
+        System.out.println("Hello , Raju ");
     }
 }
