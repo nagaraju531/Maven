b5791304c5f2ee2759d2dab7d8257fd95d618b6b
nagaraju531 <nraju531@gmail.com>
2022-04-01 16:20:52 +0530

    Update App.java

--- src/main/java/com/weblogic/app/App.java
+++ src/main/java/com/weblogic/app/App.java
@@ -8,6 +8,8 @@ public class App
     public static void main( String[] args )
     {
         
-        system.out.println("Hello , Raju ");
+        System.out.println("Hello , Raju ");
+        
+        System.out.println("Hello , Raju ");
     }
 }
